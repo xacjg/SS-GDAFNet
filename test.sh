b9a031ebef3969fc023812973a2fce84d59eb550
xacjg <33489145+xacjg@users.noreply.github.com>
2026-02-26 14:35:32 +0800

    Delete test.sh

--- test.sh
+++
@@ -1,3 +0,0 @@
-python ./tools/test.py --file_root LEVIR --lr 1e-4 --max_steps 20000
-python ./tools/test.py --file_root WHU --lr 1e-4 --max_steps 20000
-python ./tools/test.py --file_root CDD --lr 1e-4 --max_steps 20000
